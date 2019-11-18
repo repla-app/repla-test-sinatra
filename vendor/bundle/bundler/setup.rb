ea06f63cd9ade717edbb724ca6c441a5e588b18b
Roben Kleene <contact@robenkleene.com>
2019-11-18 15:50:22 -0500

    Change Gem install

--- vendor/bundle/bundler/setup.rb
+++ vendor/bundle/bundler/setup.rb
@@ -6,6 +6,6 @@ path = File.expand_path('..', __FILE__)
 $:.unshift "#{path}/"
 $:.unshift "#{path}/../#{ruby_engine}/#{ruby_version}/gems/mustermann-1.0.3/lib"
 $:.unshift "#{path}/../#{ruby_engine}/#{ruby_version}/gems/rack-2.0.7/lib"
-$:.unshift "#{path}/../#{ruby_engine}/#{ruby_version}/bundler/gems/sinatra-04a02ac2b740/rack-protection/lib"
+$:.unshift "#{path}/../#{ruby_engine}/#{ruby_version}/gems/rack-protection-2.0.7/lib"
 $:.unshift "#{path}/../#{ruby_engine}/#{ruby_version}/gems/tilt-2.0.10/lib"
-$:.unshift "#{path}/../#{ruby_engine}/#{ruby_version}/bundler/gems/sinatra-04a02ac2b740/lib"
+$:.unshift "#{path}/../#{ruby_engine}/#{ruby_version}/gems/sinatra-2.0.7/lib"
